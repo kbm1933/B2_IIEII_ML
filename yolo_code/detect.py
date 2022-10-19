f3ac38a6ef62ab933a3feb00234379e6f307479c
uniqquej <cjy4477@gmail.com>
2022-10-19 19:05:22 +0900

    이미지 색  보정 완료

--- yolo_code/detect.py
+++ yolo_code/detect.py
@@ -5,6 +5,7 @@ import pandas
 import django
 from community.models import FileUpload,YoloResult
 from PIL import Image as im
+import numpy as np
 import io
 # from django.core.files.base import ContentFile
 
@@ -41,14 +42,18 @@ def get_img(idx):
 
 # 세 번째 방법 : PIL IMAGE 모듈을 통해 이미지를 가져오는 방법. 색이 그대로 나오고 사물인식도 잘 되지만 테두리가 없음
     img_bytes = imgmodel.imgfile.read()
-    img = im.open(io.BytesIO(img_bytes))
+    img = im.open(io.BytesIO(img_bytes)) #jpg로 파일 open
 
-    results = model(img)
+    img2= np.array(img) #jpg >> numpy array로 변경
+
+    results = model(img2)
     detect = results.pandas().xyxy[0] #좌표 얻기
     print(detect)
     results.save()
 
-    img.save(f'media/YOLO/result{idx}.jpg', "JPEG")
+    final_img = im.fromarray(img2) #numpy array >> jpg로 변경
+
+    final_img.save(f'media/YOLO/result{idx}.jpg', "JPEG")
 # ==================================================================================================
 
     yolo_url = (f'YOLO/result{idx}.jpg')
